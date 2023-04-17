7320e6f68bc063da11582b97f54a4054c5d8946a
Natalia Mironova <natalymir82@gmail.com>
2023-04-17 18:28:12 +0300

    add .gitignore

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = 'https://0c35450c-8faa-4e18-92b1-399b34671a47.serverhub.praktikum-services.ru'
+CREATE_ORDERS_PATH = "/api/v1/orders"
+ORDERS_TRACK_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,16 @@
+headers = {
+    "Content-Type": "application/json"
+}
+ order_body = {
+    "firstName": "Анна",
+    "lastName": "Белкина",
+    "address": "Деревянная, 142",
+    "metroStation": 202,
+    "phone": "+99999999999",
+    "rentTime": 5,
+    "deliveryDate": "2023-0-05",
+    "comment": "Привезите, пожалуйста",
+    "color": [
+        "BLACK"
+    ]
+ }
